ed27675f7d15c3428de5714e5dbb283000dffed5
codekeyur <keyurpatel2868@gamil.com>
2023-01-27 20:46:48 -0500

    Lab-1 Creating Simple S3 bucket

---
+++ s3/main.tf
@@ -0,0 +1,47 @@
+#Lab - 1 Starting with basics, Creating Simple Private S3 bucket which has Lifecycle rule attached 
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "lab-bucket-37fbff"
+}
+
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  bucket = aws_s3_bucket.bucket.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "bucket-rule" {
+  bucket = aws_s3_bucket.bucket.bucket
+  rule {
+    id = "rule"
+    expiration {
+      days = 90
+    }
+
+    filter {
+      and {
+        prefix = "log/"
+
+        tags = {
+          rule      = "log"
+          autoclean = "true"
+        }
+      }
+    }
+    status = "Enabled"
+  }
+  
+}
